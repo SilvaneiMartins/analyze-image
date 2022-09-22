11072ef05582393e5da05caa1f94375d3b32d929
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-22 15:48:01 -0400

    Initial Commit

---
+++ analyze image/analyze_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  analyze_imageApp.swift
+//  analyze image
+//
+//  Created by Silvanei  Martins on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct analyze_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
